2f933ac581d3cc2c72c3fae6a992295743b70db9
RogueKnight096 <themasterofgames916@gmail.com>
2026-02-20 13:10:02 -0600

    Added Struct and CSV

--- Program.cs
+++ Program.cs
@@ -2,6 +2,24 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
     }
+}
+
+public struct IdentityRecord
+{
+    public string DisplayName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string WorkEmail { get; set; }
+    public string CloudLifecycleState { get; set; }
+    public string IdentityId { get; set; }
+    public bool IsManager { get; set; }
+    public string Department { get; set; }
+    public string JobTitle { get; set; }
+    public string Uid { get; set; }
+    public string AccessType { get; set; }
+    public string AccessSourceName { get; set; }
+    public string AccessDisplayName { get; set; }
+    public string AccessDescription { get; set; }
 }
