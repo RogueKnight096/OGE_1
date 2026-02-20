4332b7ca5295e186c0df44f5a332996cc36e2ab4
RogueKnight096 <themasterofgames916@gmail.com>
2026-02-20 13:44:21 -0600

    Wrote Main and Reads CSV file

--- Program.cs
+++ Program.cs
@@ -1,18 +1,55 @@
 ﻿class Program
 {
     static void Main(string[] args)
+{
+    List<Employees> employees = CSVContent();
+    Console.WriteLine($"Total records read: {employees.Count}");
+}
+
+    public static List<Employees> CSVContent()
     {
-        
+        List<Employees> employeeList = new List<Employees>();
+        using StreamReader sr = new StreamReader("Francis Tuttle Identities_Basic.csv");
+        sr.ReadLine();
+        while (!sr.EndOfStream)
+        {
+            string? line = sr.ReadLine();
+            if (line != null)
+            {
+                string[] parts = line.Split(",");
+                Employees emp = new Employees
+                {
+                    DisplayName = parts[0],
+                    FirstName = parts[1],
+                    LastName = parts[2],
+                    WorkEmail = parts[3],
+                    CloudLifecycleState = parts[4],
+                    IdentityId = parts[5],
+                    IsManager = bool.Parse(parts[6]),
+                    Department = parts[7],
+                    JobTitle = parts[8],
+                    Uid = parts[9],
+                    AccessType = parts[10],
+                    AccessSourceName = parts[11],
+                    AccessDisplayName = parts[12],
+                    AccessDescription = parts[13]
+                };
+
+                employeeList.Add(emp);
+            }
+        }
+        return employeeList;
     }
 }
 
-public struct IdentityRecord
+
+public struct Employees // Struct for all Employees
 {
     public string DisplayName { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string WorkEmail { get; set; }
-    public string CloudLifecycleState { get; set; }
+    public bool CloudLifecycleState { get; set; }
     public string IdentityId { get; set; }
     public bool IsManager { get; set; }
     public string Department { get; set; }
